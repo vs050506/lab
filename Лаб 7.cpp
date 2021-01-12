eb71cbd4e2b4873ace921d338b2f2036afeefd53
vs050506 <71372329+vs050506@users.noreply.github.com>
2021-01-12 05:00:35 +0300

    Add files via upload

--- Лаб 7.cpp
+++ Лаб 7.cpp
@@ -6,16 +6,42 @@
 —троку s1, Е, sn обработать так, чтобы все слова, состо€щие только из цифр, были удалены,
 их сумма сто€ла последним словом в строке.*/
 
+int Atoi(char* str)
+{
+	int dec = 0;
+	return dec = str[0] - '0';
+}
+
+char* Itoa(int nsum, char* mas, int sch) {
+	int count = 0;
+	do 
+	{
+		int digit = nsum % sch;
+		mas[count++] = digit + '0';
+	} while ((nsum /= sch) != 0);
+
+	mas[count] = '\0';
+
+	for (int i = 0; i < count / 2; ++i) 
+	{
+		char symbol = mas[i];
+		mas[i] = mas[count - i - 1];
+		mas[count - i - 1] = symbol;
+	}
+	return mas;
+}
+
 int main()
 {
 	setlocale(LC_ALL, "RUS");
 	char* s, * S;
+	char* l = 0;
+	char mas[50];
 	int* c;
 	int n, k, j = 0, ch = 0, za = 0;
 	bool q = 1, e = 1;
 	bool flag = 0;
-	char num1[40];
-	int num = 0;
+	int sum = 0, nsum = 0;
 	printf("¬ведите размер строки: ");
 	scanf_s("%d", &n);
 
@@ -25,6 +51,7 @@ int main()
 	printf("¬ведите строку длинной не более %d символов: ", (n - 1));
 	rewind(stdin);  /*чистка буфера*/
 	gets_s(s, n + 1); /*ввод*/
+	printf("\n");
 
 	for (int i = 0; i < n; i++)
 	{
@@ -32,7 +59,10 @@ int main()
 		{
 			for (k = i; k < n; k++)
 			{
-				if (s[k] == ' ' || s[k] == '\0') break;
+				if (s[k] == ' ' || s[k] == '\0') 
+				{
+					break;
+			    }
 				if (s[k] == '0' || s[k] == '1' || s[k] == '2' || s[k] == '3' || s[k] == '4' || s[k] == '5' || s[k] == '6' || s[k] == '7' || s[k] == '8' || s[k] == '9')
 				{
 					q = 1;
@@ -46,7 +76,10 @@ int main()
 			k = i;
 			do
 			{
-				if (s[k] == ' ') break;
+				if (s[k] == ' ') 
+				{
+					break;
+				}
 				if (s[k] == '0' || s[k] == '1' || s[k] == '2' || s[k] == '3' || s[k] == '4' || s[k] == '5' || s[k] == '6' || s[k] == '7' || s[k] == '8' || s[k] == '9')
 				{
 					e = 1;
@@ -61,20 +94,23 @@ int main()
 
 			if (q && e)
 			{
-				num1[0] = s[i];
-				num += atoi(num1);
 				c[i] = 1;
 				q = 1;
+				l = &(s[i]);
+				sum = sum * 10 + Atoi(l);
 			}
 		}
 		else
 		{
+			nsum += sum;
+			sum = 0;
 			q = 0;
 			e = 0;
 			continue;
 		}
 	}
 
+	Itoa(nsum, mas, 10);
 	int i;
 	S = (char*)calloc((n), sizeof(char));
 	for (i = 0; i < (n); i++)
@@ -86,11 +122,22 @@ int main()
 				S[j] = s[i];
 				j++;
 			}
+			if (S[j] == '\0' && S[j - 1] == '\0')
+			{
+				za = 1;
+				i = 0;
+				j--;
+				S[j] = ' ';
+				j++;
+			}
+		}
+		if (za == 1)
+		{
+			if (mas[i] == '\0') { break; }
+			S[j] = mas[i];
+			j++;
 		}
 	}
-	//sprintf_s(snum, "%d", );
-
 	printf("\n");
 	puts(S); 
-	printf(" %d", num);
 }
